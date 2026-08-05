0113839382a6a9239991d88c085ca1e6cb267bda
makoto2025 <aratamakoto2025@gmail.com>
2026-08-04 20:49:30 -0700

    feat: pagination

--- src/pages/Items.tsx
+++ src/pages/Items.tsx
@@ -3,6 +3,8 @@ import { type Product, type RawProduct } from "../types/productTypes";
 
 const Items = () => {
 	const [products, setProducts] = useState<Product[]>([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const itemsPerPage = 12;
 
 	useEffect(() => {
 		const fetchProducts = async () => {
@@ -24,12 +26,40 @@ const Items = () => {
 		fetchProducts();
 	}, []);
 
+	// Pagination
+	const indexOfLastItem = currentPage * itemsPerPage;
+	const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+	const currentItems = products.slice(indexOfFirstItem, indexOfLastItem);
+
+	const totalPages = Math.ceil(products.length / itemsPerPage);
+
 	return (
 		<div className="p-4">
 			<h1 className="text-2xl font-bold mb-4">Menu</h1>
 
+			<div className="p-4">
+				<button
+					onClick={() => setCurrentPage(currentPage - 1)}
+					disabled={currentPage === 1}
+					className="px-4 p-2 bg-gray-200 rounded disabled:opacity-50"
+				>
+					◀︎
+				</button>
+				<span className="p-2">
+					Page {currentPage} of {totalPages}
+				</span>
+				<button
+					onClick={() => setCurrentPage(currentPage + 1)}
+					disabled={currentPage === totalPages}
+					className="px-4 p-2 bg-gray-200 rounded disabled:opacity-50"
+				>
+					▶
+				</button>
+			</div>
+
 			<ul className="grid grid-cols-1 md:grid-cols-3 gap-6">
-				{products.map((p) => (
+				{currentItems.map((p) => (
 					<li key={p.id} className="border p-4 rounded shadow">
 						<img
 							src={p.photo}
