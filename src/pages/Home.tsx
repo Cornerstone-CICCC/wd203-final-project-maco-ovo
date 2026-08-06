8802da3286accbf92d352943fca9ca33b2430769
makoto2025 <aratamakoto2025@gmail.com>
2026-08-05 20:11:54 -0700

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router";
+
 const Home = () => {
 	return (
-		<>
-			<div>Home page</div>
-		</>
+		<div className="p-12 text-center">
+			<h1 className="text-4xl font-bold mb-8">Welcome</h1>
+
+			<Link
+				to="/items"
+				className="px-6 py-3 bg-amber-800 text-white font-semibold rounded hover:bg-amber-700"
+			>
+				View Menu
+			</Link>
+		</div>
 	);
 };
 
