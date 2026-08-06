b3833207a515de276d848648c2e66d66ba940e3d
makoto2025 <aratamakoto2025@gmail.com>
2026-08-05 20:12:39 -0700

    not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router";
+
 const NotFound = () => {
 	return (
-		<>
-			<div>404 NOT FOUND</div>
-		</>
+		<div className="p-12 text-center">
+			<h1 className="text-5xl font-bold text-red-500 mb-4">404</h1>
+			<h2 className="text-2xl mb-8">Page Not Found</h2>
+			<Link to="/" className="text-amber-600 underline text-lg">
+				Go back to Home
+			</Link>
+		</div>
 	);
 };
 
